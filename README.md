1645b181eb65f191ed5a260578b17b3d3633ba76
AnithaD <anitha.k.dharaneedharan@gmail.com>
2020-05-27 20:14:24 +0100

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-This project deals with basic form validation 
---
+++ precache-manifest.0e66e2e2a24186bfb5f0bd336bb6750f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a3e99dfec97156750334e90b0cb3e8",
+    "url": "/form-validation/index.html"
+  },
+  {
+    "revision": "4c1dd0208e8e89086234",
+    "url": "/form-validation/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "59e7cf751b79983cf20b",
+    "url": "/form-validation/static/css/main.c3ecbb79.chunk.css"
+  },
+  {
+    "revision": "4c1dd0208e8e89086234",
+    "url": "/form-validation/static/js/2.574463be.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/form-validation/static/js/2.574463be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59e7cf751b79983cf20b",
+    "url": "/form-validation/static/js/main.a7b969c1.chunk.js"
+  },
+  {
+    "revision": "7b42432814688ccc5672",
+    "url": "/form-validation/static/js/runtime-main.a8e79130.js"
+  }
+]);
